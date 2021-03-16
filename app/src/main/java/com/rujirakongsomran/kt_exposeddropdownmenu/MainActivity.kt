e17ad3e3d0e3daf2172f7a3ba7765f11fd04d70b
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-03-16 12:39:07 +0700

    Create ArrayAdapter

--- app/src/main/java/com/rujirakongsomran/kt_exposeddropdownmenu/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_exposeddropdownmenu/MainActivity.kt
@@ -2,6 +2,7 @@ package com.rujirakongsomran.kt_exposeddropdownmenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.rujirakongsomran.kt_exposeddropdownmenu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +15,9 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val language = resources.getStringArray(R.array.language)
+        val arrayAdapter = ArrayAdapter(applicationContext, R.layout.dropdown_item, language)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
     }
 }
