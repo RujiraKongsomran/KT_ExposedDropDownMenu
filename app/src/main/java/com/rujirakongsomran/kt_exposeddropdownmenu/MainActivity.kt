cc1b7c231548b6a1ef293ef183492d97546ab2a4
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-03-16 11:48:17 +0700

    Enable ViewBinding

--- app/src/main/java/com/rujirakongsomran/kt_exposeddropdownmenu/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_exposeddropdownmenu/MainActivity.kt
@@ -2,10 +2,17 @@ package com.rujirakongsomran.kt_exposeddropdownmenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rujirakongsomran.kt_exposeddropdownmenu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
